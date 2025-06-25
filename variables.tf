a38317498d4d086e365edf991d8580a0f9a5b165
Ubuntu <ubuntu@ip-172-31-86-12.ec2.internal>
2025-06-25 01:30:27 +0000

    terraform_complete_code

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "vpc_cidr"            { default = "196.120.0.0/16" }
+variable "public_subnet_1"     { default = "196.120.1.0/24" }
+variable "public_subnet_2"     { default = "196.120.2.0/24" }
+variable "private_subnet_1"    { default = "196.120.3.0/24" }
+variable "private_subnet_2"    { default = "196.120.4.0/24" }
+variable "eks_cluster_name"    { default = "cart-cluster" }
+variable "eks_desired_size"    { default = 2 }
+variable "eks_max_size"        { default = 2 }
+variable "eks_min_size"        { default = 1 }
+variable "db_username"         { default = "admin" }
+variable "db_password"         { default = "expenseGit12" }
